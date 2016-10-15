3fda46418b7861e5b738899da95679122592a0ab
michzio <michzio@hotmail.com>
2016-10-15 22:33:47 +0200

    added integer transfer procedures for int64, int32, int16, int8

--- transfer/integer_transfer.c
+++ transfer/integer_transfer.c
@@ -139,3 +139,136 @@ result_t recv_uint8(const sock_fd_t sock_fd, uint8_t *uint8) {
 
     return SUCCESS;
 }
+
+result_t send_int64(const sock_fd_t sock_fd, const int64_t int64) {
+
+    // convert int64 from host to network representation
+    int64_t network_int64 = htonll(int64);
+
+    ssize_t res = send(sock_fd, &network_int64, sizeof(int64_t), 0);
+    if(res < 0) {
+        fprintf(stderr, "%s: Error while sending int64 number to socket\n", __func__);
+        return FAILURE;
+    } else if(res < sizeof(int64_t)) {
+        fprintf(stderr, "%s: Error not all bytes of int64 number sent correctly to socket.\n", __func__);
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+result_t recv_int64(const sock_fd_t sock_fd, int64_t *int64) {
+
+    int64_t network_int64 = 0;
+
+    ssize_t res = recv(sock_fd, &network_int64, sizeof(int64_t), 0);
+    if(res < 0) {
+        fprintf(stderr, "%s: Error while receiving int64 number from socket.\n", __func__);
+        return FAILURE;
+    } else if(res < sizeof(int64_t)) {
+        fprintf(stderr, "%s: Error not all bytes of int64 number received from socket.\n", __func__);
+        return FAILURE;
+    }
+
+    // convert int64 from network to host representation
+    *int64 = ntohll(network_int64);
+    return SUCCESS;
+}
+
+result_t send_int32(const sock_fd_t sock_fd, const int32_t int32) {
+
+    // convert int32 from host to network representation
+    int32_t network_int32 = htonl(int32);
+
+    ssize_t res = send(sock_fd, &network_int32, sizeof(int32_t), 0);
+    if(res < 0) {
+        fprintf(stderr, "%s: Error while sending int32 number to socket.\n", __func__);
+        return FAILURE;
+    } else if(res < sizeof(int32_t)) {
+        fprintf(stderr, "%s: Error not all bytes of int32 number sent correctly to socket.\n", __func__);
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+result_t recv_int32(const sock_fd_t sock_fd, int32_t *int32) {
+
+    int32_t network_int32 = 0;
+
+    ssize_t res = recv(sock_fd, &network_int32, sizeof(int32_t), 0);
+    if(res < 0) {
+        fprintf(stderr, "%s: Error while receiving int32 number from socket.\n", __func__);
+        return FAILURE;
+    } else if(res < sizeof(int32_t)) {
+        fprintf(stderr, "%s: Error not all bytes of int32 number received form socket.\n", __func__);
+        return FAILURE;
+    }
+
+    // convert int32 from network to host representation
+    *int32 = ntohl(network_int32);
+    return SUCCESS;
+}
+
+result_t send_int16(const sock_fd_t sock_fd, const int16_t int16) {
+
+    // convert int16 from host to network representation
+    int16_t network_int16 = htons(int16);
+
+    ssize_t res = send(sock_fd, &network_int16, sizeof(int16_t), 0);
+    if(res < 0) {
+        fprintf(stderr, "%s: Error while sending int16 number to socket.\n", __func__);
+        return FAILURE;
+    } else if(res < sizeof(int16_t)) {
+        fprintf(stderr, "%s: Error not all bytes of int16 number sent correctly to socket.\n", __func__);
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+result_t recv_int16(const sock_fd_t sock_fd, int16_t *int16) {
+
+    int16_t network_int16 = 0;
+
+    ssize_t res = recv(sock_fd, &network_int16, sizeof(int16_t), 0);
+    if(res < 0) {
+        fprintf(stderr, "%s: Error while receiving int16 number from socket.\n", __func__);
+        return FAILURE;
+    } else if(res < sizeof(int16_t)) {
+        fprintf(stderr, "%s: Error not all bytes of int16 number received from socket.\n", __func__);
+        return FAILURE;
+    }
+
+    // convert int16 from network to host representation
+    *int16 = ntohs(network_int16);
+    return SUCCESS;
+}
+
+result_t send_int8(const sock_fd_t sock_fd, const int8_t int8) {
+
+    ssize_t res = send(sock_fd, &int8, sizeof(int8_t), 0);
+    if(res < 0) {
+        fprintf(stderr, "%s: Error while sending int8 number to socket.\n", __func__);
+        return FAILURE;
+    } else if(res < sizeof(int8_t)) {
+        fprintf(stderr, "%s: Error not all bytes of int8 number sent correctly to socket.\n", __func__);
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+result_t recv_int8(const sock_fd_t sock_fd, int8_t *int8) {
+
+    ssize_t res = recv(sock_fd, int8, sizeof(int8_t), 0);
+    if(res < 0) {
+        fprintf(stderr, "%s: Error while receiving int8 number from socket.\n", __func__);
+        return FAILURE;
+    } else if(res < sizeof(int8_t)) {
+        fprintf(stderr, "%s: Error not all bytes of int8 number received from socket.\n", __func__);
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
--- transfer/integer_transfer.h
+++ transfer/integer_transfer.h
@@ -18,4 +18,13 @@ result_t recv_uint16(const sock_fd_t sock_fd, uint16_t *uint16);
 result_t send_uint8(const sock_fd_t sock_fd, const uint8_t uint8);
 result_t recv_uint8(const sock_fd_t sock_fd, uint8_t *uint8);
 
+result_t send_int64(const sock_fd_t sock_fd, const int64_t int64);
+result_t recv_int64(const sock_fd_t sock_fd, int64_t *int64);
+result_t send_int32(const sock_fd_t sock_fd, const int32_t int32);
+result_t recv_int32(const sock_fd_t sock_fd, int32_t *int32);
+result_t send_int16(const sock_fd_t sock_fd, const int16_t int16);
+result_t recv_int16(const sock_fd_t sock_fd, int16_t *int16);
+result_t send_int8(const sock_fd_t sock_fd, const int8_t int8);
+result_t recv_int8(const sock_fd_t sock_fd, int8_t *int8);
+
 #endif //NETWORKING_INTEGER_TRANSFER_H
